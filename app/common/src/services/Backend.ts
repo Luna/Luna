045704eb3b01f2b5c4cc5dceefa041e39d95f7d5
Sergei Garin <sergei.garin@enso.org>
2024-10-18 13:44:43 +0300

    Replace special characters while uploading a file

--- app/common/src/services/Backend.ts
+++ app/common/src/services/Backend.ts
@@ -1378,6 +1378,13 @@ export function stripProjectExtension(name: string) {
 }
 
 /**
+ * Escape special characters in a project name to prevent them from being interpreted as path or regex
+ */
+export function escapeSpecialCharacters(name: string): string {
+  return name.replace(/[*+?^${}()|[\]\\]/g, ':')
+}
+
+/**
  * Return both the name and extension of the project file name (if any).
  * Otherwise, returns the entire name as the basename.
  */
