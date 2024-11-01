1b2aead52549392d7f3e407d71b140afa5344ed8
James Dunkerley <jdunkerley@users.noreply.github.com>
2024-11-01 12:01:11 +0000

    Support for listing accounts and properties.

--- std-bits/google-api/src/main/java/org/enso/google/GoogleAnalyticsReader.java
+++ std-bits/google-api/src/main/java/org/enso/google/GoogleAnalyticsReader.java
@@ -11,9 +11,13 @@ import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
+import java.util.TimeZone;
 
 public class GoogleAnalyticsReader {
+  /** Represents a Google Analytics account. */
   public record AnalyticsAccount(String id, String displayName, boolean deleted, ZonedDateTime created, String regionCode) {}
+
+  public record AnalyticsProperty(String id, String displayName, boolean deleted, ZonedDateTime created, String account, String currency, TimeZone timeZone) {}
 
   private static AnalyticsAdminServiceClient createAdminClient(CredentialsProvider credentialsProvider) throws IOException {
     var settings = AnalyticsAdminServiceSettings.newBuilder()
@@ -59,21 +63,54 @@ public class GoogleAnalyticsReader {
     return (limit == 0 || limit > 1000) ? 1000 : limit;
   }
 
-  /** Lists all properties of a given account. */
-  public static void listProperties(CredentialsProvider credentialsProvider, AnalyticsAccount parent, int limit, boolean includeDeleted) throws IOException {
-    int pageSize = getPageSize(limit);
-    try (var client = createAdminClient(credentialsProvider)) {
-      var request = ListPropertiesRequest
-          .newBuilder()
-          .setPageSize(pageSize)
-          .setShowDeleted(includeDeleted)
-          .build();
+  /**
+   * Lists all properties of a given account.
+   *
+   * @param credentialsProvider the credentials provider
+   * @param parents the parent accounts or null for all properties
+   *               (e.g. "accounts/123" for account with ID 123)
+   * @param limit the maximum number of properties to return
+   *              (0 for all properties, up to 1000)
+   * @param includeDeleted whether to include deleted properties
+   * @return an array of properties
+   */
+  public static AnalyticsProperty[] listProperties(CredentialsProvider credentialsProvider, AnalyticsAccount[] parents, int limit, boolean includeDeleted) throws IOException {
+    if (parents != null) {
+      parents = listAccounts(credentialsProvider, 0, false);
+    }
 
-      var response = client.listProperties(request);
-      for (var page : response.iteratePages()) {
-        for (var property : page.iterateAll()) {
+    if (parents == null || parents.length == 0) {
+      return new AnalyticsProperty[0];
+    }
+
+    int pageSize = getPageSize(limit);
+    var output = new ArrayList<AnalyticsProperty>(pageSize);
+    try (var client = createAdminClient(credentialsProvider)) {
+      for (var parent : parents) {
+        var request = ListPropertiesRequest
+            .newBuilder()
+            .setPageSize(pageSize)
+            .setShowDeleted(includeDeleted)
+            .setFilter("parent: " + parent.id());
+
+        var response = client.listProperties(request.build());
+        for (var page : response.iteratePages()) {
+          for (var property : page.iterateAll()) {
+            var ensoProperty = new AnalyticsProperty(
+                property.getName(),
+                property.getDisplayName(),
+                property.hasDeleteTime(),
+                Instant.ofEpochSecond(property.getCreateTime().getSeconds(), property.getCreateTime().getNanos()).atZone(ZoneId.systemDefault()),
+                property.getAccount(),
+                property.getCurrencyCode(),
+                TimeZone.getTimeZone(property.getTimeZone())
+            );
+            output.add(ensoProperty);
+          }
         }
       }
+
+      return output.toArray(new AnalyticsProperty[0]);
     }
   }
 }
