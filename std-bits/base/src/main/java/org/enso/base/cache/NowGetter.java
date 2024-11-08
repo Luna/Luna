4a87a2591448815c5cc61acfa7e6e5027a53546a
Gregory Travis <greg.travis@enso.org>
2024-11-08 15:04:51 -0600

    fix now

--- std-bits/base/src/main/java/org/enso/base/cache/NowGetter.java
+++ std-bits/base/src/main/java/org/enso/base/cache/NowGetter.java
@@ -3,7 +3,16 @@ package org.enso.base.cache;
 import java.time.ZonedDateTime;
 
 public class NowGetter extends Mockable<ZonedDateTime> {
-    public NowGetter() {
-        super(() -> ZonedDateTime.now());
-    }
+  public NowGetter() {
+      super(() -> ZonedDateTime.now());
+  }
+
+  /**
+   * This is necessary because a direct call to the superclass does not convert
+   * Value to ZonedDateTime.
+   */
+  @Override
+  public void mocked(ZonedDateTime dt) {
+    super.mocked(dt);
+  }
 }
