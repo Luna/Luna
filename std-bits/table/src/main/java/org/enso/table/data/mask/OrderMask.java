03613526d3ac3b53ea8e0d2be80c434e8a3a64f9
Radosław Waśko <radoslaw.wasko@enso.org>
2024-11-13 11:55:21 +0100

    WIP

--- std-bits/table/src/main/java/org/enso/table/data/mask/OrderMask.java
+++ std-bits/table/src/main/java/org/enso/table/data/mask/OrderMask.java
@@ -14,7 +14,8 @@ public interface OrderMask {
    * org.enso.table.data.storage.Storage.NOT_FOUND_INDEX}, in which case a missing value should be
    * inserted at this position.
    *
-   * <p>Indices may appear zero or multiple times in the mask - meaning rows that will be gone or duplicated.
+   * <p>Indices may appear zero or multiple times in the mask - meaning rows that will be gone or
+   * duplicated.
    */
   int get(int idx);
 
