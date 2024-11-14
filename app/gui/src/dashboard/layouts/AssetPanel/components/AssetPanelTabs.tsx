036c6f1794f6667fb76465d96e25b293487c556e
Sergei Garin <sergei.garin@enso.org>
2024-11-15 00:12:27 +0300

    Fix bg

--- app/gui/src/dashboard/layouts/AssetPanel/components/AssetPanelTabs.tsx
+++ app/gui/src/dashboard/layouts/AssetPanel/components/AssetPanelTabs.tsx
@@ -83,7 +83,7 @@ export const AssetPanelTab = memo(function AssetPanelTab(props: AssetPanelTabPro
                   variants={{ active: { opacity: 1 }, inactive: { opacity: 0 } }}
                   initial="inactive"
                   animate={!isActive && isHovered ? 'active' : 'inactive'}
-                  className="bg-inv absolute inset-x-1.5 inset-y-1.5 -z-1 rounded-full transition-colors duration-300"
+                  className="absolute inset-x-1.5 inset-y-1.5 -z-1 rounded-full bg-invert transition-colors duration-300"
                 />
 
                 <div
