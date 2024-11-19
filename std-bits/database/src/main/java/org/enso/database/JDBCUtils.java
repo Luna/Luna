47b64574ed70e47876e527a2bff2798b579438fe
Adam Riley <riley.ad@gmail.com>
2024-11-19 12:41:16 +0200

    sbt javafmtAll

--- std-bits/database/src/main/java/org/enso/database/JDBCUtils.java
+++ std-bits/database/src/main/java/org/enso/database/JDBCUtils.java
@@ -3,7 +3,6 @@ package org.enso.database;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Timestamp;
 import java.sql.Types;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
