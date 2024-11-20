b2b606e611e40128f502a93ad554ae3f376b185c
Radosław Waśko <radoslaw.wasko@enso.org>
2024-11-20 11:38:01 +0100

    wip

--- std-bits/table/src/main/java/org/enso/table/data/column/storage/type/StorageType.java
+++ std-bits/table/src/main/java/org/enso/table/data/column/storage/type/StorageType.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZonedDateTime;
+
 import org.enso.base.polyglot.NumericConverter;
 
 /**
@@ -21,6 +22,7 @@ public sealed interface StorageType
         DateType,
         FloatType,
         IntegerType,
+        NullType,
         TextType,
         TimeOfDayType {
   /**
