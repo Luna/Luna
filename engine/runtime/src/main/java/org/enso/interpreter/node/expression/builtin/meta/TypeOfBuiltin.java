12ed8a3b23786574e0fe692b1c9ccac272cf651e
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-11-21 16:29:40 +0100

    Type of a value with warning shouldn't be wrapped in a warning

--- engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/meta/TypeOfBuiltin.java
+++ engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/meta/TypeOfBuiltin.java
@@ -1,10 +1,13 @@
 package org.enso.interpreter.node.expression.builtin.meta;
 
 import com.oracle.truffle.api.dsl.GenerateUncached;
+import com.oracle.truffle.api.interop.UnsupportedMessageException;
 import com.oracle.truffle.api.nodes.Node;
 import org.enso.interpreter.dsl.AcceptsError;
+import org.enso.interpreter.dsl.AcceptsWarning;
 import org.enso.interpreter.dsl.BuiltinMethod;
 import org.enso.interpreter.runtime.library.dispatch.TypeOfNode;
+import org.enso.interpreter.runtime.warning.WarningsLibrary;
 
 @BuiltinMethod(
     type = "Meta",
@@ -13,11 +16,19 @@ import org.enso.interpreter.runtime.library.dispatch.TypeOfNode;
     autoRegister = false)
 @GenerateUncached
 public final class TypeOfBuiltin extends Node {
+  private @Child WarningsLibrary warnings = WarningsLibrary.getFactory().createDispatched(11);
   private @Child TypeOfNode typeOf = TypeOfNode.create();
 
   private TypeOfBuiltin() {}
 
-  public Object execute(@AcceptsError Object value) {
+  public Object execute(@AcceptsError @AcceptsWarning Object value) {
+    if (warnings.hasWarnings(value)) {
+      try {
+        value = warnings.removeWarnings(value);
+      } catch (UnsupportedMessageException ex) {
+        // keep the old value
+      }
+    }
     return typeOf.findTypeOrError(value);
   }
 
