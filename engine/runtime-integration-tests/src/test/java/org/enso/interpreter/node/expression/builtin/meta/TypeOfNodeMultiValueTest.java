9de6986d088294d916ffaf5bb8dbfb04d677c045
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-11-21 16:21:01 +0100

    Test checking behavior of Enso multi value with respect to their types

--- engine/runtime-integration-tests/src/test/java/org/enso/interpreter/node/expression/builtin/meta/TypeOfNodeMultiValueTest.java
+++ engine/runtime-integration-tests/src/test/java/org/enso/interpreter/node/expression/builtin/meta/TypeOfNodeMultiValueTest.java
@@ -1,25 +1,30 @@
 package org.enso.interpreter.node.expression.builtin.meta;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import com.oracle.truffle.api.interop.TruffleObject;
 import java.util.ArrayList;
 import org.enso.interpreter.runtime.callable.UnresolvedConstructor;
 import org.enso.interpreter.runtime.callable.UnresolvedSymbol;
+import org.enso.interpreter.runtime.data.EnsoMultiValue;
+import org.enso.interpreter.runtime.data.Type;
 import org.enso.interpreter.runtime.error.DataflowError;
 import org.enso.interpreter.runtime.library.dispatch.TypeOfNode;
 import org.enso.interpreter.test.ValuesGenerator;
 import org.enso.test.utils.ContextUtils;
 import org.enso.test.utils.TestRootNode;
 import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Value;
 import org.junit.AfterClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 @RunWith(Parameterized.class)
-public class TypeOfNodeTest {
+public class TypeOfNodeMultiValueTest {
   @Parameterized.Parameter(0)
   public Object value;
 
@@ -48,18 +53,30 @@ public class TypeOfNodeTest {
     main = typ
     """);
     var data = new ArrayList<Object[]>();
-    for (var v : g.allValues()) {
-      var t = typeOf.execute(v);
-      if (!v.isNull()) {
-        assertTrue("Type of " + v + " is " + t, t.isMetaObject());
-        var n = t.getMetaSimpleName();
-        var raw = ContextUtils.unwrapValue(ctx(), v);
-        data.add(new Object[] {raw, n});
-      }
+    for (var polyValue : g.allValues()) {
+      registerValue(typeOf, polyValue, data);
     }
     data.add(new Object[] {UnresolvedSymbol.build("unknown_name", null), "Function"});
     data.add(new Object[] {UnresolvedConstructor.build(null, "Unknown_Name"), "Function"});
     return data.toArray(new Object[0][]);
+  }
+
+  private static void registerValue(Value typeOf, Value polyValue, ArrayList<Object[]> data) {
+    var t = typeOf.execute(polyValue);
+    if (!polyValue.isNull()) {
+      assertTrue("Type of " + polyValue + " is " + t, t.isMetaObject());
+      var rawValue = ContextUtils.unwrapValue(ctx(), polyValue);
+      var rawType = ContextUtils.unwrapValue(ctx(), t);
+      if (rawType instanceof Type type) {
+        var multi = EnsoMultiValue.create(new Type[] {type}, new Object[] {rawValue});
+        var n = t.getMetaSimpleName();
+        data.add(new Object[] {multi, n});
+      } else {
+        if (!t.isHostObject()) {
+          data.add(new Object[] {rawValue, null});
+        }
+      }
+    }
   }
 
   @AfterClass
@@ -80,12 +97,27 @@ public class TypeOfNodeTest {
     assertType(value, type, true);
   }
 
-  private static void assertType(Object symbol, String expectedTypeName, boolean withPriming) {
+  private static void assertType(Object value, String expectedTypeName, boolean withPriming) {
+    assertNotNull("Value " + value + " should have a type", expectedTypeName);
     ContextUtils.executeInContext(
         ctx(),
         () -> {
           var node = TypeOfNode.create();
-          var root = new TestRootNode((frame) -> node.findTypeOrError(frame.getArguments()[0]));
+          var root =
+              new TestRootNode(
+                  (frame) -> {
+                    var arg = frame.getArguments()[0];
+                    var t = node.findTypeOrError(arg);
+                    var all = node.findAllTypes(arg);
+                    if (t instanceof DataflowError) {
+                      assertNull("No types for errors", all);
+                    } else {
+                      assertNotNull("All types found for " + value, all);
+                      assertEquals("Size is one", 1, all.length);
+                      assertEquals("Same type for " + value, t, all[0]);
+                    }
+                    return t;
+                  });
           root.insertChildren(node);
           var call = root.getCallTarget();
 
@@ -95,7 +127,7 @@ public class TypeOfNodeTest {
             assertTrue(
                 "Empty foreign is unknown: " + foreignType, foreignType instanceof DataflowError);
           }
-          var symbolType = call.call(symbol);
+          var symbolType = call.call(value);
           var symbolTypeValue = ctx.asValue(symbolType);
           assertTrue("It is meta object: " + symbolTypeValue, symbolTypeValue.isMetaObject());
           assertEquals(expectedTypeName, symbolTypeValue.getMetaSimpleName());
