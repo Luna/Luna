a8ab6d4011efe4a5f55758bb6e1e00a07d8ac34f
Kaz <kaz@lambdaverse.org>
2024-11-25 08:16:12 -0800

    Don't use `defer`

--- app/gui/src/project-view/components/ConditionalTeleport.vue
+++ app/gui/src/project-view/components/ConditionalTeleport.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 /**
- * @file This component is equivalent to `Teleport` (with `defer` enabled) except when `disabled` is `true`, if `to`
- * doesn't specify a valid teleportation target, Vue won't crash.
+ * @file This component is equivalent to `Teleport` except when `disabled` is `true`, `to` doesn't have to specify a
+ * valid teleportation target (works around a Vue issue present at least in 3.5.2-3.5.13).
  */
 import { type RendererElement } from 'vue'
 
@@ -12,7 +12,8 @@ const { disabled } = defineProps<{
 </script>
 
 <template>
-  <Teleport v-if="!disabled" defer :to="to">
+  <!-- Note: `defer` must not be used here, or Vue errors will occur when unmounting components as of 3.5.2. -->
+  <Teleport v-if="!disabled" :to="to">
     <slot />
   </Teleport>
   <slot v-else />
