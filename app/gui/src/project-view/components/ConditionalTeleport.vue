335d6d4c12cb043719fe4e928dbf18c16db2674c
Kaz <kaz@lambdaverse.org>
2024-11-25 07:36:11 -0800

    Fix Vue crash

---
+++ app/gui/src/project-view/components/ConditionalTeleport.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+/**
+ * @file This component is equivalent to `Teleport` (with `defer` enabled) except when `disabled` is `true`, if `to`
+ * doesn't specify a valid teleportation target, Vue won't crash.
+ */
+import { type RendererElement } from 'vue'
+
+const { disabled } = defineProps<{
+  disabled: boolean
+  to: string | RendererElement | null | undefined
+}>()
+</script>
+
+<template>
+  <Teleport v-if="!disabled" defer :to="to">
+    <slot />
+  </Teleport>
+  <slot v-else />
+</template>
