6a1e89fe4b68e19725a571adc15e13585f44a9e8
Gregory Travis <greg.travis@enso.org>
2024-11-26 14:37:43 -0500

    docs

--- std-bits/base/src/main/java/org/enso/base/cache/ReloadDetector.java
+++ std-bits/base/src/main/java/org/enso/base/cache/ReloadDetector.java
@@ -5,13 +5,17 @@ import org.graalvm.polyglot.Value;
 
 /**
  * Detects that the reload button has been pressed.
- * 
+ *
  * .hasReloadOccurred() returns true if the reload button was pressed since the
  * last call to .hasReloadOccurred().
  * 
+ * This uses a weak reference (created in eval'd Enso code) that is set to null
+ * on reload.
+ *
  */
 
 public class ReloadDetector {
+    // Weak reference that is set to null on reload.
     private Value triggerRef;
 
     public ReloadDetector() {
