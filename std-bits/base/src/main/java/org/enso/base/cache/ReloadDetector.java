5f176e7a2fcf2208f31c1ac6cfadd37dd9b6c436
Gregory Travis <greg.travis@enso.org>
2024-11-27 11:37:14 -0500

    fmt

--- std-bits/base/src/main/java/org/enso/base/cache/ReloadDetector.java
+++ std-bits/base/src/main/java/org/enso/base/cache/ReloadDetector.java
@@ -6,38 +6,35 @@ import org.graalvm.polyglot.Value;
 /**
  * Detects that the reload button has been pressed.
  *
- * .hasReloadOccurred() returns true if the reload button was pressed since the
- * last call to .hasReloadOccurred().
- * 
- * This uses a weak reference (created in eval'd Enso code) that is set to null
- * on reload.
+ * <p>.hasReloadOccurred() returns true if the reload button was pressed since the last call to
+ * .hasReloadOccurred().
  *
+ * <p>This uses a weak reference (created in eval'd Enso code) that is set to null on reload.
  */
-
 public class ReloadDetector {
-    // Weak reference that is set to null on reload.
-    private Value triggerRef;
+  // Weak reference that is set to null on reload.
+  private Value triggerRef;
 
-    public ReloadDetector() {
-        resetTriggerRef();
+  public ReloadDetector() {
+    resetTriggerRef();
+  }
+
+  public boolean hasReloadOccurred() {
+    var reloadHasOccurred = triggerRef.invokeMember("get").isNull();
+    if (reloadHasOccurred) {
+      resetTriggerRef();
     }
+    return reloadHasOccurred;
+  }
 
-    public boolean hasReloadOccurred() {
-        var reloadHasOccurred = triggerRef.invokeMember("get").isNull();
-        if (reloadHasOccurred) {
-            resetTriggerRef();
-        }
-        return reloadHasOccurred;
-    }
-
-    private void resetTriggerRef() {
-      // The `0` value stored in the reference is not used; it just has to
-      // something other than null.
-      var module =
-          Context.getCurrent()
-              .eval(
-                  "enso",
-                  """
+  private void resetTriggerRef() {
+    // The `0` value stored in the reference is not used; it just has to
+    // something other than null.
+    var module =
+        Context.getCurrent()
+            .eval(
+                "enso",
+                """
       import Standard.Base.Runtime.Ref.Ref
       import Standard.Base.Data.Boolean.Boolean
       import Standard.Base.Nothing.Nothing
@@ -53,10 +50,10 @@ public class ReloadDetector {
 
           clear self = self.ref.put Nothing
       """);
-      triggerRef = module.invokeMember("eval_expression", "Trigger.new");
-    }
+    triggerRef = module.invokeMember("eval_expression", "Trigger.new");
+  }
 
-    void simulateReloadTestOnly() {
-        triggerRef.invokeMember("clear");
-    }
+  void simulateReloadTestOnly() {
+    triggerRef.invokeMember("clear");
+  }
 }
