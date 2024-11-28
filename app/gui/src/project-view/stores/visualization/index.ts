dcf020d1fbdb244ab6a2cabdd97c249a7aea52f3
Paweł Grabarz <frizi09@gmail.com>
2024-11-28 13:57:43 +0100

    format

--- app/gui/src/project-view/stores/visualization/index.ts
+++ app/gui/src/project-view/stores/visualization/index.ts
@@ -78,8 +78,9 @@ const builtinVisualizationsByName = Object.fromEntries(
   builtinVisualizations.map((viz) => [viz.name, viz]),
 )
 
-export const [provideVisualizationStore, useVisualizationStore] =
-  createContextStore('visualization', (proj: ProjectStore) => {
+export const [provideVisualizationStore, useVisualizationStore] = createContextStore(
+  'visualization',
+  (proj: ProjectStore) => {
     const cache = reactive(new Map<VisualizationId, Promise<VisualizationModule>>())
     /**
      * A map from file path to {@link AbortController}, so that a file change event can stop previous
@@ -283,4 +284,5 @@ export const [provideVisualizationStore, useVisualizationStore] =
     }
 
     return { types, get, icon }
-  })
+  },
+)
