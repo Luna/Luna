da2d4388a7a97b274f3130e12b53e2a49f03c214
Jaroslav Tulach <jaroslav.tulach@enso.org>
2024-11-28 08:02:29 +0100

    Ref.new takes only one argument (again)

--- std-bits/base/src/main/java/org/enso/base/enso_cloud/EnsoSecretHelper.java
+++ std-bits/base/src/main/java/org/enso/base/enso_cloud/EnsoSecretHelper.java
@@ -194,7 +194,7 @@ public final class EnsoSecretHelper extends SecretValueResolver {
           private Value ref:Ref
 
           new obj -> Cache =
-            ref = Ref.new obj Boolean.True
+            ref = Ref.new obj
             Cache.Value ref
 
           get self = self.ref.get
