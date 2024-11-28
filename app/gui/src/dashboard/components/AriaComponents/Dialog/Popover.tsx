459d41741d809b64fcf19f572da3df6c69d669c6
Sergei Garin <sergei.garin@enso.org>
2024-11-28 16:32:16 +0300

    Remove unrelated change

--- app/gui/src/dashboard/components/AriaComponents/Dialog/Popover.tsx
+++ app/gui/src/dashboard/components/AriaComponents/Dialog/Popover.tsx
@@ -20,12 +20,11 @@ import * as variants from './variants'
 
 /** Props for a {@link Popover}. */
 export interface PopoverProps
-  extends Omit<aria.PopoverProps, 'children' | 'defaultOpen'>,
+  extends Omit<aria.PopoverProps, 'children'>,
     twv.VariantProps<typeof POPOVER_STYLES> {
   readonly children:
     | React.ReactNode
     | ((opts: aria.PopoverRenderProps & { readonly close: () => void }) => React.ReactNode)
-  readonly isDismissable?: boolean
 }
 
 export const POPOVER_STYLES = twv.tv({
@@ -48,19 +47,21 @@ export const POPOVER_STYLES = twv.tv({
     },
     rounded: {
       none: { base: 'rounded-none', dialog: 'rounded-none' },
-      small: { base: 'rounded-sm', dialog: 'rounded-sm scroll-offset-edge-md' },
-      medium: { base: 'rounded-md', dialog: 'rounded-md scroll-offset-edge-xl' },
-      large: { base: 'rounded-lg', dialog: 'rounded-lg scroll-offset-edge-xl' },
-      xlarge: { base: 'rounded-xl', dialog: 'rounded-xl scroll-offset-edge-xl' },
-      xxlarge: { base: 'rounded-2xl', dialog: 'rounded-2xl scroll-offset-edge-2xl' },
-      xxxlarge: { base: 'rounded-3xl', dialog: 'rounded-3xl scroll-offset-edge-3xl' },
-      xxxxlarge: { base: 'rounded-4xl', dialog: 'rounded-4xl scroll-offset-edge-4xl' },
+      small: { base: 'rounded-sm', dialog: 'rounded-sm' },
+      medium: { base: 'rounded-md', dialog: 'rounded-md' },
+      large: { base: 'rounded-lg', dialog: 'rounded-lg' },
+      xlarge: { base: 'rounded-xl', dialog: 'rounded-xl' },
+      xxlarge: { base: 'rounded-2xl', dialog: 'rounded-2xl' },
+      xxxlarge: { base: 'rounded-3xl', dialog: 'rounded-3xl' },
+      xxxxlarge: { base: 'rounded-4xl', dialog: 'rounded-4xl' },
     },
   },
   slots: {
-    dialog: variants.DIALOG_BACKGROUND({ class: 'flex-auto overflow-y-auto max-h-[inherit]' }),
+    dialog: variants.DIALOG_BACKGROUND({
+      class: 'flex-auto overflow-y-auto max-h-[inherit]',
+    }),
   },
-  defaultVariants: { rounded: 'xxxlarge', size: 'small' },
+  defaultVariants: { rounded: 'xxlarge', size: 'small' },
 })
 
 const SUSPENSE_LOADER_PROPS = { minHeight: 'h32' } as const
@@ -75,12 +76,10 @@ export function Popover(props: PopoverProps) {
     className,
     size,
     rounded,
-    isDismissable = true,
     placement = 'bottom start',
     ...ariaPopoverProps
   } = props
 
-  const popoverRef = React.useRef<HTMLDivElement>(null)
   const dialogRef = React.useRef<HTMLDivElement>(null)
   // We use as here to make the types more accurate
   // eslint-disable-next-line no-restricted-syntax
@@ -96,26 +95,16 @@ export function Popover(props: PopoverProps) {
   })
 
   utlities.useInteractOutside({
-    ref: popoverRef,
+    ref: dialogRef,
     id: dialogId,
-    onInteractOutside: useEventCallback(() => {
-      if (isDismissable) {
-        close()
-      } else {
-        if (popoverRef.current) {
-          // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-          utlities.animateScale(popoverRef.current, 1.02)
-        }
-      }
-    }),
+    onInteractOutside: close,
   })
 
+  const dialogContextValue = React.useMemo(() => ({ close, dialogId }), [close, dialogId])
   const popoverStyle = React.useMemo(() => ({ zIndex: '' }), [])
 
   return (
     <aria.Popover
-      ref={popoverRef}
-      {...ariaPopoverProps}
       className={(values) =>
         POPOVER_STYLES({
           isEntering: values.isEntering,
@@ -129,25 +118,24 @@ export function Popover(props: PopoverProps) {
       placement={placement}
       style={popoverStyle}
       shouldCloseOnInteractOutside={() => false}
+      {...ariaPopoverProps}
     >
       {(opts) => (
-        <aria.FocusScope restoreFocus contain={!opts.isExiting}>
-          <dialogStackProvider.DialogStackRegistrar id={dialogId} type="popover" />
-
+        <dialogStackProvider.DialogStackRegistrar id={dialogId} type="popover">
           <div
             id={dialogId}
             ref={dialogRef}
             className={POPOVER_STYLES({ ...opts, size, rounded }).dialog()}
           >
-            <errorBoundary.ErrorBoundary>
-              <suspense.Suspense loaderProps={SUSPENSE_LOADER_PROPS}>
-                <dialogProvider.DialogProvider close={close} dialogId={dialogId}>
+            <dialogProvider.DialogProvider value={dialogContextValue}>
+              <errorBoundary.ErrorBoundary>
+                <suspense.Suspense loaderProps={SUSPENSE_LOADER_PROPS}>
                   {typeof children === 'function' ? children({ ...opts, close }) : children}
-                </dialogProvider.DialogProvider>
-              </suspense.Suspense>
-            </errorBoundary.ErrorBoundary>
+                </suspense.Suspense>
+              </errorBoundary.ErrorBoundary>
+            </dialogProvider.DialogProvider>
           </div>
-        </aria.FocusScope>
+        </dialogStackProvider.DialogStackRegistrar>
       )}
     </aria.Popover>
   )
