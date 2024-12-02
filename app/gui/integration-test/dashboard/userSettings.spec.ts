c7f4ea0bd4763bba7c06f9a61e10b123c0cccac6
somebody1234 <ehern.lee@gmail.com>
2024-12-02 19:47:06 +1000

    Update `userSettings.spec` to use new API

--- app/gui/integration-test/dashboard/userSettings.spec.ts
+++ app/gui/integration-test/dashboard/userSettings.spec.ts
@@ -3,71 +3,71 @@ import * as test from '@playwright/test'
 
 import * as actions from './actions'
 
-test.test('user settings', async ({ page }) => {
-  const api = await actions.mockAllAndLoginAndExposeAPI({ page })
-  const localActions = actions.settings.userAccount
-  test.expect(api.currentUser()?.name).toBe(api.defaultName)
+const NEW_USERNAME = 'another user-name'
+const NEW_PASSWORD = '1234!' + actions.VALID_PASSWORD
 
-  await localActions.go(page)
-  const nameInput = localActions.locateNameInput(page)
-  const newName = 'another user-name'
-  await nameInput.fill(newName)
-  await nameInput.press('Enter')
-  test.expect(api.currentUser()?.name).toBe(newName)
-  test.expect(api.currentOrganization()?.name).not.toBe(newName)
-})
+test.test('user settings', ({ page }) =>
+  actions
+    .mockAllAndLogin({ page })
+    .do((_, { api }) => {
+      test.expect(api.currentUser()?.name).toBe(api.defaultName)
+    })
+    .goToPage.settings()
+    .accountForm()
+    .fillName(NEW_USERNAME)
+    .save()
+    .do((_, { api }) => {
+      test.expect(api.currentUser()?.name).toBe(NEW_USERNAME)
+      test.expect(api.currentOrganization()?.name).not.toBe(NEW_USERNAME)
+    }),
+)
 
 test.test('change password form', async ({ page }) => {
-  const api = await actions.mockAllAndLoginAndExposeAPI({ page })
-  const localActions = actions.settings.changePassword
-
-  await localActions.go(page)
-  test.expect(api.currentPassword()).toBe(actions.VALID_PASSWORD)
-  await localActions.locateCurrentPasswordInput(page).fill(actions.VALID_PASSWORD)
-  await localActions.locateNewPasswordInput(page).fill(actions.INVALID_PASSWORD)
-
-  await test.test.step('Invalid new password', async () => {
-    await localActions.locateCurrentPasswordInput(page).fill(actions.VALID_PASSWORD)
-    await localActions.locateNewPasswordInput(page).fill(actions.INVALID_PASSWORD)
-    await localActions.locateConfirmNewPasswordInput(page).fill(actions.INVALID_PASSWORD)
-    await localActions.locateSaveButton(page).click()
-    await test
-      .expect(
-        localActions
-          .locate(page)
-          .getByRole('group', { name: /^New password/, exact: true })
-          .locator('.text-danger')
-          .last(),
-      )
-      .toHaveText(actions.TEXT.passwordValidationError)
-  })
-
-  await test.test.step('Invalid new password confirmation', async () => {
-    await localActions.locateCurrentPasswordInput(page).fill(actions.VALID_PASSWORD)
-    await localActions.locateNewPasswordInput(page).fill(actions.VALID_PASSWORD)
-    await localActions.locateConfirmNewPasswordInput(page).fill(actions.VALID_PASSWORD + 'a')
-    await localActions.locateSaveButton(page).click()
-    await test
-      .expect(
-        localActions
-          .locate(page)
-          .getByRole('group', { name: /^Confirm new password/, exact: true })
-          .locator('.text-danger')
-          .last(),
-      )
-      .toHaveText(actions.TEXT.passwordMismatchError)
-  })
-
-  await test.test.step('Successful password change', async () => {
-    const newPassword = '1234!' + actions.VALID_PASSWORD
-    await localActions.locateNewPasswordInput(page).fill(newPassword)
-    await localActions.locateConfirmNewPasswordInput(page).fill(newPassword)
-    await localActions.locateSaveButton(page).click()
-    await test.expect(localActions.locateCurrentPasswordInput(page)).toHaveText('')
-    await test.expect(localActions.locateNewPasswordInput(page)).toHaveText('')
-    await test.expect(localActions.locateConfirmNewPasswordInput(page)).toHaveText('')
-    test.expect(api.currentPassword()).toBe(newPassword)
-  })
+  actions
+    .mockAllAndLogin({ page })
+    .do((_, { api }) => {
+      test.expect(api.currentPassword()).toBe(actions.VALID_PASSWORD)
+    })
+    .goToPage.settings()
+    .changePasswordForm()
+    .fillCurrentPassword(actions.VALID_PASSWORD)
+    .fillNewPassword(actions.INVALID_PASSWORD)
+    .fillConfirmNewPassword(actions.INVALID_PASSWORD)
+    .save()
+    .step('Invalid new password', async (page) => {
+      await test
+        .expect(
+          page
+            .getByRole('group', { name: /^New password/, exact: true })
+            .locator('.text-danger')
+            .last(),
+        )
+        .toHaveText(actions.TEXT.passwordValidationError)
+    })
+    .changePasswordForm()
+    .fillCurrentPassword(actions.VALID_PASSWORD)
+    .fillNewPassword(actions.VALID_PASSWORD)
+    .fillConfirmNewPassword(actions.VALID_PASSWORD + 'a')
+    .save()
+    .step('Invalid new password confirmation', async (page) => {
+      await test
+        .expect(
+          page
+            .getByRole('group', { name: /^Confirm new password/, exact: true })
+            .locator('.text-danger')
+            .last(),
+        )
+        .toHaveText(actions.TEXT.passwordMismatchError)
+    })
+    .changePasswordForm()
+    .fillCurrentPassword(actions.VALID_PASSWORD)
+    .fillNewPassword(NEW_PASSWORD)
+    .fillConfirmNewPassword(NEW_PASSWORD)
+    .save()
+    // TODO: consider checking that password inputs are now empty.
+    .step('Successful password change', (_, { api }) => {
+      test.expect(api.currentPassword()).toBe(NEW_PASSWORD)
+    })
 })
 
 test.test('upload profile picture', async ({ page }) => {
