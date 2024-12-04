088b61416b93bf1d2b63dc49da81361f058db682
Pavel Marek <pavel.marek@enso.org>
2024-12-04 21:34:43 +0100

    Remove all test interfaces using IRNode annotation

--- engine/runtime-parser-processor-tests/src/main/java/org/enso/runtime/parser/processor/test/gen/ir/module/JScope.java
+++
@@ -1,8 +0,0 @@
-package org.enso.runtime.parser.processor.test.gen.ir.module;
-
-import org.enso.compiler.core.IR;
-import org.enso.runtime.parser.dsl.IRNode;
-
-/** A representation of constructs that can only occur in the top-level module scope */
-@IRNode
-public interface JScope extends IR {}
