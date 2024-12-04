11d2a257b0e59501cd69da90d34191fd4bcaa9c6
Pavel Marek <pavel.marek@enso.org>
2024-12-04 21:59:21 +0100

    Update TestIRPRocessorInline.
    Remove IRNode annotation

--- engine/runtime-parser-processor-tests/src/test/java/org/enso/runtime/parser/processor/test/TestIRProcessorInline.java
+++ engine/runtime-parser-processor-tests/src/test/java/org/enso/runtime/parser/processor/test/TestIRProcessorInline.java
@@ -3,7 +3,6 @@ package org.enso.runtime.parser.processor.test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
 
 import com.google.testing.compile.CompilationSubject;
 import com.google.testing.compile.Compiler;
@@ -47,15 +46,14 @@ public class TestIRProcessorInline {
   }
 
   @Test
-  public void simpleIRNodeWithoutChildren_CompilationSucceeds() {
+  public void simpleIRNodeWithoutFields_CompilationSucceeds() {
     var src =
         JavaFileObjects.forSourceString(
             "JName",
             """
-        import org.enso.runtime.parser.dsl.IRNode;
-        import org.enso.compiler.core.IR;
-        @IRNode
-        public interface JName extends IR {}
+        import org.enso.runtime.parser.dsl.GenerateIR;
+        @GenerateIR
+        public final class JName {}
         """);
     var compiler = Compiler.javac().withProcessors(new IRProcessor());
     var compilation = compiler.compile(src);
@@ -63,113 +61,36 @@ public class TestIRProcessorInline {
   }
 
   @Test
-  public void annotatedInterfaceMustExtendIR() {
+  public void onlyFinalClassCanBeAnnotated() {
     var src =
         JavaFileObjects.forSourceString(
-            "Hello",
+            "JName",
             """
-        import org.enso.runtime.parser.dsl.IRNode;
-        @IRNode
-        public interface Hello {}
-        """);
-    var compiler = Compiler.javac().withProcessors(new IRProcessor());
-    var compilation = compiler.compile(src);
-    CompilationSubject.assertThat(compilation).failed();
-  }
-
-  @Test
-  public void annotatedInterfaceMustNotExtendTwoIRInterfaces() {
-    var src =
-        JavaFileObjects.forSourceString(
-            "Hello",
-            """
-        import org.enso.runtime.parser.dsl.IRNode;
-        import org.enso.compiler.core.IR;
-        import org.enso.compiler.core.ir.Expression;
-
-        @IRNode
-        public interface Hello extends IR, Expression {}
+        import org.enso.runtime.parser.dsl.GenerateIR;
+        @GenerateIR
+        public class JName {}
         """);
     var compiler = Compiler.javac().withProcessors(new IRProcessor());
     var compilation = compiler.compile(src);
     CompilationSubject.assertThat(compilation).failed();
     CompilationSubject.assertThat(compilation).hadErrorCount(1);
-    CompilationSubject.assertThat(compilation)
-        .hadErrorContaining("must extend only a single IR interface");
+    CompilationSubject.assertThat(compilation).hadErrorContaining("final");
   }
 
   @Test
-  public void annotationCanOnlyBeAppliedToInterface() {
+  public void simpleIRNodeWithUserDefinedFiled_CompilationSucceeds() {
     var src =
-        JavaFileObjects.forSourceString(
-            "Hello",
-            """
-        import org.enso.runtime.parser.dsl.IRNode;
-        @IRNode
-        public class Hello {}
-        """);
-    var compiler = Compiler.javac().withProcessors(new IRProcessor());
-    var compilation = compiler.compile(src);
-    CompilationSubject.assertThat(compilation).failed();
-  }
-
-  @Test
-  public void childAnnotation_MustBeAppliedToIRField() {
-    expectCompilationFailure(
         """
-      import org.enso.runtime.parser.dsl.IRNode;
-      import org.enso.runtime.parser.dsl.IRChild;
-      import org.enso.compiler.core.IR;
+        import org.enso.runtime.parser.dsl.GenerateIR;
 
-      @IRNode
-      public interface MyIR extends IR {
-        @IRChild String expression();
-      }
-      """);
-  }
-
-  @Test
-  public void simpleIRNodeWithoutChildren_GeneratesSource() {
-    var src =
-        JavaFileObjects.forSourceString(
-            "JName",
-            """
-        import org.enso.runtime.parser.dsl.IRNode;
-        import org.enso.compiler.core.IR;
-        @IRNode
-        public interface JName extends IR {}
-        """);
-    var compiler = Compiler.javac().withProcessors(new IRProcessor());
-    var compilation = compiler.compile(src);
-    CompilationSubject.assertThat(compilation).succeeded();
-    CompilationSubject.assertThat(compilation).generatedSourceFile("JNameGen").isNotNull();
-    var srcSubject =
-        CompilationSubject.assertThat(compilation)
-            .generatedSourceFile("JNameGen")
-            .contentsAsUtf8String();
-    srcSubject.containsMatch("");
-    var genSrc = compilation.generatedSourceFile("JNameGen");
-    assertThat(genSrc.isPresent(), is(true));
-    assertThat("Generated just one source", compilation.generatedSourceFiles().size(), is(1));
-  }
-
-  @Test
-  public void doesNotOverrideStaticParameterlessMethod() {
-    var src =
-        generatedClass(
-            "Hello",
-            """
-        import org.enso.runtime.parser.dsl.IRNode;
-        import org.enso.compiler.core.IR;
-
-        @IRNode
-        public interface Hello extends IR {
-          static String name() {
-            return "Hello";
-          }
+        @GenerateIR
+        public final class JName {
+          public JName(String name) {}
         }
-        """);
-    assertThat(src, not(containsString("\"Hello\"")));
+        """;
+    var genClass = generatedClass("JName", src);
+    assertThat(genClass, containsString("class JNameGen"));
+    assertThat("Getter for 'name' generated", genClass, containsString("String name()"));
   }
 
   @Test
@@ -178,14 +99,13 @@ public class TestIRProcessorInline {
         generatedClass(
             "MyIR",
             """
-        import org.enso.runtime.parser.dsl.IRNode;
+        import org.enso.runtime.parser.dsl.GenerateIR;
         import org.enso.runtime.parser.dsl.IRChild;
-        import org.enso.compiler.core.IR;
         import org.enso.compiler.core.ir.Expression;
 
-        @IRNode
-        public interface MyIR extends IR {
-          @IRChild Expression expression();
+        @GenerateIR
+        public final class MyIR {
+          public MyIR(@IRChild Expression expression) {}
         }
         """);
     assertThat(genSrc, containsString("Expression expression()"));
@@ -197,16 +117,36 @@ public class TestIRProcessorInline {
         generatedClass(
             "MyIR",
             """
-        import org.enso.runtime.parser.dsl.IRNode;
+        import org.enso.runtime.parser.dsl.GenerateIR;
         import org.enso.runtime.parser.dsl.IRChild;
-        import org.enso.compiler.core.IR;
 
-        @IRNode
-        public interface MyIR extends IR {
-          boolean suspended();
+        @GenerateIR
+        public final class MyIR {
+          public MyIR(boolean suspended) {}
         }
         """);
     assertThat(genSrc, containsString("boolean suspended()"));
+  }
+
+  @Test
+  public void interfacesMustBeSubtypeOfIR() {
+    var src =
+        JavaFileObjects.forSourceString(
+            "MyIR",
+            """
+        import org.enso.runtime.parser.dsl.GenerateIR;
+
+        interface MySuperIR {
+          boolean suspended();
+        }
+
+        @GenerateIR(interfaces = {"MySuperIR"})
+        public final class MyIR { }
+  """);
+    var compiler = Compiler.javac().withProcessors(new IRProcessor());
+    var compilation = compiler.compile(src);
+    CompilationSubject.assertThat(compilation).failed();
+    CompilationSubject.assertThat(compilation).hadErrorContaining("subtype");
   }
 
   @Test
@@ -215,17 +155,15 @@ public class TestIRProcessorInline {
         generatedClass(
             "MyIR",
             """
-        import org.enso.runtime.parser.dsl.IRNode;
-        import org.enso.runtime.parser.dsl.IRChild;
+        import org.enso.runtime.parser.dsl.GenerateIR;
         import org.enso.compiler.core.IR;
 
         interface MySuperIR extends IR {
           boolean suspended();
         }
 
-        @IRNode
-        public interface MyIR extends MySuperIR {
-        }
+        @GenerateIR(interfaces = {"MySuperIR"})
+        public final class MyIR { }
 
         """);
     assertThat(src, containsString("boolean suspended()"));
@@ -237,17 +175,16 @@ public class TestIRProcessorInline {
         generatedClass(
             "MyIR",
             """
-        import org.enso.runtime.parser.dsl.IRNode;
-        import org.enso.runtime.parser.dsl.IRChild;
+        import org.enso.runtime.parser.dsl.GenerateIR;
         import org.enso.compiler.core.IR;
 
         interface MySuperIR extends IR {
           boolean suspended();
         }
 
-        @IRNode
-        public interface MyIR extends MySuperIR {
-          boolean suspended();
+        @GenerateIR
+        public final class MyIR {
+          public MyIR(boolean suspended) {}
         }
 
         """);
@@ -260,8 +197,7 @@ public class TestIRProcessorInline {
         generatedClass(
             "MyIR",
             """
-        import org.enso.runtime.parser.dsl.IRNode;
-        import org.enso.runtime.parser.dsl.IRChild;
+        import org.enso.runtime.parser.dsl.GenerateIR;
         import org.enso.compiler.core.IR;
 
         interface MySuperSuperIR extends IR {
@@ -271,299 +207,53 @@ public class TestIRProcessorInline {
         interface MySuperIR extends MySuperSuperIR {
         }
 
-        @IRNode
-        public interface MyIR extends MySuperIR {
+        @GenerateIR
+        public final class MyIR {
         }
         """);
     assertThat(src, containsString("boolean suspended()"));
   }
 
   @Test
-  public void irNodeAsNestedInterface() {
+  public void irNodeAsNestedClass() {
     var src =
         generatedClass(
             "JName",
             """
-        import org.enso.runtime.parser.dsl.IRNode;
+        import org.enso.runtime.parser.dsl.GenerateIR;
         import org.enso.compiler.core.IR;
 
-        @IRNode
         public interface JName extends IR {
           String name();
 
-          interface JBlank extends JName {}
+          @GenerateIR(interfaces = {"JName"})
+          final class JBlank {}
         }
         """);
-    assertThat(src, containsString("public final class JNameGen"));
-    assertThat(src, containsString("public static final class JBlankGen implements JName.JBlank"));
+    assertThat(src, containsString("class JBlankGen implements JName"));
   }
 
   @Test
-  public void returnValueCanBeScalaList() {
+  public void fieldCanBeScalaList() {
     var src =
         generatedClass(
             "JName",
             """
-        import org.enso.runtime.parser.dsl.IRNode;
+        import org.enso.runtime.parser.dsl.GenerateIR;
         import org.enso.runtime.parser.dsl.IRChild;
         import org.enso.compiler.core.IR;
         import scala.collection.immutable.List;
 
-        @IRNode
-        public interface JName extends IR {
-          @IRChild
-          List<IR> expressions();
+        @GenerateIR
+        public final class JName {
+          public JName(@IRChild List<IR> expressions) {}
         }
         """);
-    assertThat(src, containsString("public final class JNameGen"));
+    assertThat(src, containsString("class JNameGen"));
     assertThat(src, containsString("List<IR> expressions"));
   }
 
-  @Test
-  public void processorDoesNotGenerateOverridenMethods() {
-    var src =
-        generatedClass(
-            "JName",
-            """
-        import org.enso.runtime.parser.dsl.IRNode;
-        import org.enso.compiler.core.IR;
+  // TODO: Can contain multiple GenerateIR annotations in single source
 
-        @IRNode
-        public interface JName extends IR {
-          String name();
-
-          interface JQualified extends JName {
-            @Override
-            default String name() {
-              return null;
-            }
-          }
-        }
-        """);
-    assertThat(src, containsString("public final class JNameGen"));
-    assertThat(src, not(containsString("String name()")));
-  }
-
-  @Test
-  public void overrideCorrectMethods() {
-    var src =
-        generatedClass(
-            "JExpression",
-            """
-        import org.enso.runtime.parser.dsl.IRNode;
-        import org.enso.compiler.core.IR;
-
-        @IRNode
-        public interface JExpression extends IR {
-
-          interface JBlock extends JExpression {
-            boolean suspended();
-          }
-
-          interface JBinding extends JExpression {
-            String name();
-          }
-        }
-        """);
-    assertThat(src, containsString("class JBlockGen"));
-    assertThat(src, containsString("class JBindingGen"));
-  }
-
-  @Test
-  public void canOverrideMethodsFromIR() {
-    var src =
-        generatedClass(
-            "JName",
-            """
-        import org.enso.runtime.parser.dsl.IRNode;
-        import org.enso.compiler.core.IR;
-
-        @IRNode
-        public interface JName extends IR {
-          @Override
-          JName duplicate(boolean keepLocations, boolean keepMetadata, boolean keepDiagnostics, boolean keepIdentifiers);
-
-          interface JSelf extends JName {}
-        }
-        """);
-    assertThat(src, containsString("JName duplicate"));
-    assertThat(src, containsString("JSelfGen"));
-  }
-
-  @Test
-  public void canDefineCopyMethod_WithUserDefinedField() {
-    var genSrc =
-        generatedClass(
-            "JName",
-            """
-        import org.enso.runtime.parser.dsl.IRNode;
-        import org.enso.runtime.parser.dsl.IRCopyMethod;
-        import org.enso.compiler.core.IR;
-
-        @IRNode
-        public interface JName extends IR {
-          String nameField();
-
-          @IRCopyMethod
-          JName copy(String nameField);
-        }
-        """);
-    assertThat(genSrc, containsString("JName copy(String nameField"));
-  }
-
-  @Test
-  public void canDefineCopyMethod_WithMetaField() {
-    var genSrc =
-        generatedClass(
-            "JName",
-            """
-        import org.enso.runtime.parser.dsl.IRNode;
-        import org.enso.runtime.parser.dsl.IRCopyMethod;
-        import org.enso.compiler.core.IR;
-        import org.enso.compiler.core.ir.MetadataStorage;
-
-        @IRNode
-        public interface JName extends IR {
-          String nameField();
-
-          @IRCopyMethod
-          JName copy(MetadataStorage passData);
-        }
-        """);
-    assertThat(genSrc, containsString("JName copy(MetadataStorage"));
-  }
-
-  @Test
-  public void canDefineMultipleCopyMethods() {
-    var genSrc =
-        generatedClass(
-            "JName",
-            """
-        import org.enso.runtime.parser.dsl.IRNode;
-        import org.enso.runtime.parser.dsl.IRCopyMethod;
-        import org.enso.compiler.core.IR;
-        import org.enso.compiler.core.ir.MetadataStorage;
-
-        @IRNode
-        public interface JName extends IR {
-          String nameField();
-
-          @IRCopyMethod
-          JName copy(MetadataStorage passData);
-
-          @IRCopyMethod
-          JName copy(String nameField);
-        }
-        """);
-    assertThat(genSrc, containsString("JName copy(MetadataStorage"));
-    assertThat(genSrc, containsString("JName copy(String"));
-  }
-
-  @Test
-  public void copyMethod_WithArbitraryArgumentOrder() {
-    var genSrc =
-        generatedClass(
-            "JName",
-            """
-        import org.enso.runtime.parser.dsl.IRNode;
-        import org.enso.runtime.parser.dsl.IRCopyMethod;
-        import org.enso.compiler.core.IR;
-        import org.enso.compiler.core.ir.MetadataStorage;
-        import org.enso.compiler.core.ir.DiagnosticStorage;
-
-        @IRNode
-        public interface JName extends IR {
-          String nameField();
-
-          @IRCopyMethod
-          JName copy(String nameField, MetadataStorage passData, DiagnosticStorage diagnostics);
-        }
-        """);
-    assertThat(genSrc, containsString("JName copy("));
-  }
-
-  @Test
-  public void copyMethod_MustContainValidFieldsAsParameters_1() {
-    expectCompilationFailure(
-        """
-      import org.enso.runtime.parser.dsl.IRNode;
-      import org.enso.runtime.parser.dsl.IRCopyMethod;
-      import org.enso.compiler.core.IR;
-      import org.enso.compiler.core.ir.MetadataStorage;
-      import org.enso.compiler.core.ir.DiagnosticStorage;
-
-      @IRNode
-      public interface JName extends IR {
-        String nameField();
-
-        @IRCopyMethod
-        JName copy(String NON_EXISTING_FIELD_NAME);
-      }
-      """);
-  }
-
-  @Test
-  public void copyMethod_MustContainValidFieldsAsParameters_2() {
-    expectCompilationFailure(
-        """
-      import org.enso.runtime.parser.dsl.IRNode;
-      import org.enso.runtime.parser.dsl.IRCopyMethod;
-      import org.enso.compiler.core.IR;
-      import org.enso.compiler.core.ir.MetadataStorage;
-      import org.enso.compiler.core.ir.DiagnosticStorage;
-
-      @IRNode
-      public interface JName extends IR {
-        String nameField();
-
-        @IRCopyMethod
-        JName copy(String nameField, String ANOTHER_NON_EXISTING);
-      }
-      """);
-  }
-
-  @Test
-  public void copyMethod_WithMoreFieldsOfSameType() {
-    var genSrc =
-        generatedClass(
-            "JName",
-            """
-        import org.enso.runtime.parser.dsl.IRNode;
-        import org.enso.runtime.parser.dsl.IRCopyMethod;
-        import org.enso.compiler.core.IR;
-        import org.enso.compiler.core.ir.MetadataStorage;
-        import org.enso.compiler.core.ir.DiagnosticStorage;
-
-        @IRNode
-        public interface JName extends IR {
-          String nameField_1();
-          String nameField_2();
-
-          @IRCopyMethod
-          JName copy(String nameField_1, String nameField_2);
-        }
-        """);
-    assertThat(genSrc, containsString("JName copy("));
-  }
-
-  @Test
-  public void mapExpressions_CanOverride() {
-    var genSrc =
-        generatedClass(
-            "JExpression",
-            """
-        import org.enso.runtime.parser.dsl.IRNode;
-        import org.enso.compiler.core.IR;
-        import org.enso.compiler.core.ir.Expression;
-        import java.util.function.Function;
-
-        @IRNode
-        public interface JExpression extends IR {
-
-          @Override
-          JExpression mapExpressions(Function<Expression, Expression> fn);
-        }
-        """);
-    assertThat(genSrc, containsString("JExpression mapExpressions("));
-  }
+  // TODO: Multiple interfaces in the annotation
 }
