f6f0e4dacfef45554b210012e3350e14bc003a94
Pavel Marek <pavel.marek@enso.org>
2024-12-04 21:32:58 +0100

    Revert "Empty extends Expression"
    This reverts commit 4a0ea14a3752dcc149c704f856059a2368cf7ad2.

--- engine/runtime-parser/src/main/java/org/enso/compiler/core/ir/Empty.java
+++ engine/runtime-parser/src/main/java/org/enso/compiler/core/ir/Empty.java
@@ -1,11 +1,12 @@
 package org.enso.compiler.core.ir;
 
 import java.util.UUID;
+import org.enso.compiler.core.IR;
 import org.enso.runtime.parser.dsl.IRCopyMethod;
 import org.enso.runtime.parser.dsl.IRNode;
 
 @IRNode
-public interface Empty extends Expression {
+public interface Empty extends IR {
   static Empty createEmpty() {
     return EmptyGen.builder().build();
   }
@@ -14,15 +15,13 @@ public interface Empty extends Expression {
     return EmptyGen.builder().location(location).build();
   }
 
-  static Empty createFromLocationAndPassData(
-      IdentifiedLocation location, MetadataStorage passData) {
-    return EmptyGen.builder().location(location).passData(passData).build();
+  static Empty createFromLocationAndPassData(IdentifiedLocation location, MetadataStorage passData) {
+    return EmptyGen.builder()
+        .location(location)
+        .passData(passData)
+        .build();
   }
 
   @IRCopyMethod
-  Empty copy(
-      IdentifiedLocation location,
-      MetadataStorage passData,
-      DiagnosticStorage diagnostics,
-      UUID id);
+  Empty copy(IdentifiedLocation location, MetadataStorage passData, DiagnosticStorage diagnostics, UUID id);
 }
