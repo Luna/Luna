21afb5276d76a81da97fb4dccd7638050304b1a2
Dmitry Bushev <bushevdv@gmail.com>
2024-12-09 15:49:10 +0000

    update: akka-wrapper

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,8 +14,17 @@ object Dependencies {
 
   /** Manually updated versions. */
   object VersionsPinned {
+    // Akka
     val akkaActor = "2.6.20"
+    val akkaHttp = "10.2.10"
+    val javaDiffUtils = "4.12"
+    val protobufJava = "3.25.1"
+    val reactiveStreams = "1.0.3"
+    val scalaParserCombinators = "1.1.2"
+    val sprayJson = "1.3.6"
+    // GraalVM
     val graalMavenPackages = "24.0.0"
+    // Scala
     val scala = "2.13.15"
   }
 
@@ -26,6 +35,13 @@ object Dependencies {
     */
   object Versions {
     val akkaActor = "2.8.8"
+    val akkaHttp = "10.5.3"
+    val akkaHttpCore = "10.5.3"
+    val akkaHttpSprayJson = "10.5.3"
+    val akkaParsing = "10.5.3"
+    val akkaProtobufV3 = "2.8.8"
+    val akkaSlf4j = "2.8.8"
+    val akkaStream = "2.8.8"
     val catsCore = "2.12.0"
     val catsKernel = "2.10.0"
     val circeCore = "0.14.10"
@@ -37,7 +53,7 @@ object Dependencies {
     val commonsIo = "2.18.0"
     val commonsLang3 = "3.17.0"
     val config = "1.4.3"
-    val javaDiffUtils = "4.12"
+    val javaDiffUtils = "4.15"
     val jline = "3.27.1"
     val jna = "5.15.0"
     val jsoniterScalaCore = "2.28.5"
@@ -48,8 +64,12 @@ object Dependencies {
     val logbackCore = "1.5.12"
     val orgNetbeansModulesSampler = "RELEASE230"
     val orgOpenideUtilLookup = "RELEASE230"
+    val protobufJava = "4.29.1"
+    val reactiveStreams = "1.0.4"
     val scalaCompiler = "2.13.15"
+    val scalaJava8Compat = "1.0.2"
     val scalaLogging = "3.9.5"
+    val scalaParserCombinators = "2.4.0"
     val scalacheck = "1.18.1"
     val scalatest = "3.2.19"
     val sentry = "7.18.1"
@@ -57,6 +77,7 @@ object Dependencies {
     val shapeless = "2.3.10"
     val slf4jApi = "2.0.16"
     val snakeyaml = "2.3"
+    val sprayJson = "1.3.6"
     val svm = "24.1.1"
   }
 
@@ -76,6 +97,13 @@ object Dependencies {
     */
   object Compile {
     val akkaActor = "com.typesafe.akka" %% "akka-actor" % VersionsPinned.akkaActor
+    val akkaHttp = "com.typesafe.akka" %% "akka-http" % VersionsPinned.akkaHttp
+    val akkaHttpCore = "com.typesafe.akka" %% "akka-http-core" % VersionsPinned.akkaHttp
+    val akkaSlf4j = "com.typesafe.akka" %% "akka-slf4j" % VersionsPinned.akkaActor
+    val akkaStream = "com.typesafe.akka" %% "akka-stream" % VersionsPinned.akkaActor
+    val akkaParsing = "com.typesafe.akka" %% "akka-parsing" % VersionsPinned.akkaHttp
+    val akkaProtobufV3 = "com.typesafe.akka" %% "akka-protobuf-v3" % VersionsPinned.akkaActor
+    val akkaHttpSprayJson = "com.typesafe.akka" %% "akka-http-spray-json" % VersionsPinned.akkaHttp
     val catsCore = "org.typelevel" %% "cats-core" % Versions.catsCore
     val catsKernel = "org.typelevel" %% "cats-kernel" % Versions.catsKernel
     val circeGeneric = "io.circe" %% "circe-generic" % Versions.circeGeneric
@@ -86,7 +114,7 @@ object Dependencies {
     val commonsCompress = "org.apache.commons" % "commons-compress" % Versions.commonsCompress
     val commonsIo = "commons-io" % "commons-io" % Versions.commonsIo
     val commonsLang3 = "org.apache.commons" % "commons-lang3" % Versions.commonsLang3
-    val javaDiffUtils = "io.github.java-diff-utils" % "java-diff-utils" % Versions.javaDiffUtils
+    val javaDiffUtils = "io.github.java-diff-utils" % "java-diff-utils" % VersionsPinned.javaDiffUtils
     val jline = "org.jline" % "jline" % Versions.jline
     val jna = "net.java.dev.jna" % "jna" % Versions.jna
     val jsoniterScalaMacros =
@@ -100,17 +128,23 @@ object Dependencies {
     val netbeansModulesSampler =
       "org.netbeans.api" % "org-netbeans-modules-sampler" % Versions.orgNetbeansModulesSampler
     val openideUtilLookup = "org.netbeans.api" % "org-openide-util-lookup" % Versions.orgOpenideUtilLookup
+    val protobufJava = "com.google.protobuf" % "protobuf-java" % VersionsPinned.protobufJava
+    val reactiveStreams = "org.reactivestreams" % "reactive-streams" % VersionsPinned.reactiveStreams
     val sbtJunitInterface = "com.github.sbt" % "junit-interface" % Versions.junitInterface
     val scalaCompiler = "org.scala-lang" % "scala-compiler" % VersionsPinned.scala
     val scalaLibrary = "org.scala-lang" % "scala-library" % VersionsPinned.scala
     val scalaReflect = "org.scala-lang" % "scala-reflect" % VersionsPinned.scala
     val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % Versions.scalaLogging
+    val scalaParserCombinators =
+      "org.scala-lang.modules" %% "scala-parser-combinators" % VersionsPinned.scalaParserCombinators
+    val scalaJava8Compat = "org.scala-lang.modules" %% "scala-java8-compat" % Versions.scalaJava8Compat
     val scalatest = "org.scalatest" %% "scalatest" % Versions.scalatest
     val sentryLogback = "io.sentry" % "sentry-logback" % Versions.sentryLogback
     val sentry = "io.sentry" % "sentry" % Versions.sentry
     val shapeless = "com.chuusai" %% "shapeless" % Versions.shapeless
     val slf4jApi = "org.slf4j" % "slf4j-api" % Versions.slf4jApi
     val snakeyaml = "org.yaml" % "snakeyaml" % Versions.snakeyaml
+    val sprayJson = "io.spray" %% "spray-json" % VersionsPinned.sprayJson
     val typesafeConfig = "com.typesafe" % "config" % Versions.config
   }
 
