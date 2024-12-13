0d92891b8eecd3071f0f4f1d8c55524b637d14a8
Pavel Marek <pavel.marek@enso.org>
2024-12-13 14:49:36 +0100

    Add BuiltinsJavaInteropTest

---
+++ engine/runtime-integration-tests/src/test/java/org/enso/example/PolyglotTestClass.java
@@ -0,0 +1,18 @@
+package org.enso.example;
+
+import java.time.LocalDate;
+import org.graalvm.polyglot.Value;
+
+public class PolyglotTestClass {
+  public static boolean isPolyglotDate_Object(Object obj) {
+    return obj instanceof Value polyglotVal && polyglotVal.isDate();
+  }
+
+  public static boolean isPolyglotDate_LocalDate(LocalDate date) {
+    return date != null;
+  }
+
+  public static boolean isPolyglotDate_Value(Value val) {
+    return val != null && val.isDate();
+  }
+}
