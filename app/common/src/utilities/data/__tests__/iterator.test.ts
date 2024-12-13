cfe0306221b95367aa6f8a4a9518b3eaaa0b4c06
somebody1234 <ehern.lee@gmail.com>
2024-12-14 03:17:57 +1000

    Switch back to namespace import for `iter`

--- app/common/src/utilities/data/__tests__/iterator.test.ts
+++ app/common/src/utilities/data/__tests__/iterator.test.ts
@@ -1,17 +1,6 @@
 import { expect, test } from 'vitest'
 
-import {
-  chain,
-  count as countFn,
-  empty,
-  filter,
-  filterDefined,
-  last as lastFn,
-  range,
-  tryGetSoleValue,
-  zip,
-  zipLongest,
-} from '../iter'
+import * as iter from '../iter'
 
 interface IteratorCase<T> {
   iterable: Iterable<T>
@@ -24,42 +13,42 @@ interface IteratorCase<T> {
 function makeCases(): IteratorCase<unknown>[] {
   return [
     {
-      iterable: empty(),
+      iterable: iter.empty(),
       soleValue: undefined,
       first: undefined,
       last: undefined,
       count: 0,
     },
     {
-      iterable: chain(empty(), empty()),
+      iterable: iter.chain(iter.empty(), iter.empty()),
       soleValue: undefined,
       first: undefined,
       last: undefined,
       count: 0,
     },
     {
-      iterable: chain(empty(), ['a'], empty()),
+      iterable: iter.chain(iter.empty(), ['a'], iter.empty()),
       soleValue: 'a',
       first: 'a',
       last: 'a',
       count: 1,
     },
     {
-      iterable: range(10, 11),
+      iterable: iter.range(10, 11),
       soleValue: 10,
       first: 10,
       last: 10,
       count: 1,
     },
     {
-      iterable: range(10, 20),
+      iterable: iter.range(10, 20),
       soleValue: undefined,
       first: 10,
       last: 19,
       count: 10,
     },
     {
-      iterable: range(20, 10),
+      iterable: iter.range(20, 10),
       soleValue: undefined,
       first: 20,
       last: 11,
@@ -87,56 +76,56 @@ function makeCases(): IteratorCase<unknown>[] {
       count: 2,
     },
     {
-      iterable: filterDefined([undefined, 'a', undefined, 'b', undefined]),
+      iterable: iter.filterDefined([undefined, 'a', undefined, 'b', undefined]),
       soleValue: undefined,
       first: 'a',
       last: 'b',
       count: 2,
     },
     {
-      iterable: filter([7, 'a', 8, 'b', 9], el => typeof el === 'string'),
+      iterable: iter.filter([7, 'a', 8, 'b', 9], el => typeof el === 'string'),
       soleValue: undefined,
       first: 'a',
       last: 'b',
       count: 2,
     },
     {
-      iterable: zip(['a', 'b'], range(1, 2)),
+      iterable: iter.zip(['a', 'b'], iter.range(1, 2)),
       soleValue: ['a', 1],
       first: ['a', 1],
       last: ['a', 1],
       count: 1,
     },
     {
-      iterable: zip(['a', 'b'], range(1, 3)),
+      iterable: iter.zip(['a', 'b'], iter.range(1, 3)),
       soleValue: undefined,
       first: ['a', 1],
       last: ['b', 2],
       count: 2,
     },
     {
-      iterable: zip(['a', 'b'], range(1, 4)),
+      iterable: iter.zip(['a', 'b'], iter.range(1, 4)),
       soleValue: undefined,
       first: ['a', 1],
       last: ['b', 2],
       count: 2,
     },
     {
-      iterable: zipLongest(['a', 'b'], range(1, 2)),
+      iterable: iter.zipLongest(['a', 'b'], iter.range(1, 2)),
       soleValue: undefined,
       first: ['a', 1],
       last: ['b', undefined],
       count: 2,
     },
     {
-      iterable: zipLongest(['a', 'b'], range(1, 3)),
+      iterable: iter.zipLongest(['a', 'b'], iter.range(1, 3)),
       soleValue: undefined,
       first: ['a', 1],
       last: ['b', 2],
       count: 2,
     },
     {
-      iterable: zipLongest(['a', 'b'], range(1, 4)),
+      iterable: iter.zipLongest(['a', 'b'], iter.range(1, 4)),
       soleValue: undefined,
       first: ['a', 1],
       last: [undefined, 3],
@@ -146,13 +135,13 @@ function makeCases(): IteratorCase<unknown>[] {
 }
 
 test.each(makeCases())('tryGetSoleValue: case %#', ({ iterable, soleValue }) => {
-  expect(tryGetSoleValue(iterable)).toEqual(soleValue)
+  expect(iter.tryGetSoleValue(iterable)).toEqual(soleValue)
 })
 
 test.each(makeCases())('last: case %#', ({ iterable, last }) => {
-  expect(lastFn(iterable)).toEqual(last)
+  expect(iter.last(iterable)).toEqual(last)
 })
 
 test.each(makeCases())('count: case %#', ({ iterable, count }) => {
-  expect(countFn(iterable)).toEqual(count)
+  expect(iter.count(iterable)).toEqual(count)
 })
