a9bc6a26a37bf4be16a09eb9610dbbcd063dfea2
Gregory Travis <greg.travis@enso.org>
2024-12-13 15:10:54 -0500

    clear excel cache on reload

--- std-bits/table/src/main/java/org/enso/table/excel/ExcelConnectionPool.java
+++ std-bits/table/src/main/java/org/enso/table/excel/ExcelConnectionPool.java
@@ -20,6 +20,7 @@ import org.apache.poi.openxml4j.opc.PackageAccess;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.enso.base.cache.ReloadDetector;
 
 public class ExcelConnectionPool {
   public static final ExcelConnectionPool INSTANCE = new ExcelConnectionPool();
@@ -34,6 +35,8 @@ public class ExcelConnectionPool {
             "Cannot open a read-only Excel connection while an Excel file is being "
                 + "written to. This is a bug in the Table library.");
       }
+
+      clearOnReload();
 
       if (!file.exists()) {
         throw new FileNotFoundException(file.toString());
@@ -207,6 +210,29 @@ public class ExcelConnectionPool {
 
   private final HashMap<String, ConnectionRecord> records = new HashMap<>();
   private boolean isCurrentlyWriting = false;
+
+  /** Used to clear the ConnectionRecord on reload. */
+  private final ReloadDetector reloadDetector = new ReloadDetector();
+
+  /** If a reload has just happened, clear the ConnectionRecord cache. */
+  private void clearOnReload() throws IOException {
+    if (reloadDetector.hasReloadOccurred()) {
+      for (var record : records.values()) {
+        record.close();
+      }
+      records.clear();
+    }
+  }
+
+  /** Public for testing. */
+  public int getConnectionRecordCount() {
+    return records.size();
+  }
+
+  /** Public for testing. */
+  public void simulateReloadTestOnly() {
+    reloadDetector.simulateReloadTestOnly();
+  }
 
   static class ConnectionRecord {
     private int refCount;
