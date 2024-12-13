d00282af06e072902072069f36356906031422b7
somebody1234 <ehern.lee@gmail.com>
2024-12-13 16:48:58 +1000

    Edit text for "delete asset" modal

--- app/gui/src/dashboard/modals/ConfirmDeleteModal.tsx
+++ app/gui/src/dashboard/modals/ConfirmDeleteModal.tsx
@@ -11,29 +11,34 @@ import { useText } from '#/providers/TextProvider'
 
 /** Props for a {@link ConfirmDeleteModal}. */
 export interface ConfirmDeleteModalProps {
-  readonly defaultOpen?: boolean
+  readonly defaultOpen?: boolean | undefined
+  readonly cannotUndo?: boolean | undefined
   /** Must fit in the sentence "Are you sure you want to <action>?". */
   readonly actionText: string
   /** The label shown on the colored confirmation button. "Delete" by default. */
-  readonly actionButtonLabel?: string
+  readonly actionButtonLabel?: string | undefined
   readonly doDelete: () => void
 }
 
 /** A modal for confirming the deletion of an asset. */
 export default function ConfirmDeleteModal(props: ConfirmDeleteModalProps) {
-  const { defaultOpen, actionText, actionButtonLabel = 'Delete', doDelete } = props
+  const {
+    defaultOpen = false,
+    cannotUndo = false,
+    actionText,
+    actionButtonLabel = 'Delete',
+    doDelete,
+  } = props
 
   const { unsetModal } = useSetModal()
   const { getText } = useText()
 
   return (
-    <Dialog
-      title={getText('areYouSure')}
-      role="alertdialog"
-      modalProps={defaultOpen == null ? {} : { defaultOpen }}
-    >
+    <Dialog title={getText('areYouSure')} role="alertdialog" modalProps={{ defaultOpen }}>
       <Form schema={z.object({})} method="dialog" onSubmit={doDelete} onSubmitSuccess={unsetModal}>
-        <Text className="relative">{getText('confirmPrompt', actionText)}</Text>
+        <Text className="relative">
+          {getText(cannotUndo ? 'confirmPromptCannotUndo' : 'confirmPrompt', actionText)}
+        </Text>
 
         <ButtonGroup>
           <Form.Submit variant="delete" className="relative">
