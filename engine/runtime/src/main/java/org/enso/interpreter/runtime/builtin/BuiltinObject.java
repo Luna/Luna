78002d1c49df5f67f276ee7b56e094e534d6771d
Pavel Marek <pavel.marek@enso.org>
2024-12-16 11:37:46 +0100

    Revert "[WIP] Try to remove builtin type caching in BuiltinObject"
    This reverts commit 75109063c5ca5084d2de8734594dc2ae06402a20.

--- engine/runtime/src/main/java/org/enso/interpreter/runtime/builtin/BuiltinObject.java
+++ engine/runtime/src/main/java/org/enso/interpreter/runtime/builtin/BuiltinObject.java
@@ -2,7 +2,6 @@ package org.enso.interpreter.runtime.builtin;
 
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
-import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.dsl.Bind;
 import com.oracle.truffle.api.interop.InteropLibrary;
 import com.oracle.truffle.api.interop.UnsupportedMessageException;
@@ -28,6 +27,8 @@ public abstract class BuiltinObject extends EnsoObject {
 
   private final String builtinName;
 
+  @CompilationFinal private Builtin cachedBuiltinType;
+
   /**
    * @param builtinName Simple name of the builtin that should be contained in {@link
    *     org.enso.interpreter.runtime.builtin.Builtins#builtinsByName}.
@@ -42,11 +43,13 @@ public abstract class BuiltinObject extends EnsoObject {
   }
 
   @ExportMessage
-  @TruffleBoundary
   public final Type getType(@Bind("$node") Node node) {
-    var ctx = EnsoContext.get(node);
-    var builtinType = ctx.getBuiltins().getBuiltinType(builtinName);
-    return builtinType.getType();
+    if (cachedBuiltinType == null) {
+      CompilerDirectives.transferToInterpreterAndInvalidate();
+      var ctx = EnsoContext.get(node);
+      cachedBuiltinType = ctx.getBuiltins().getBuiltinType(builtinName);
+    }
+    return cachedBuiltinType.getType();
   }
 
   /**
@@ -73,7 +76,6 @@ public abstract class BuiltinObject extends EnsoObject {
   }
 
   @ExportMessage
-  @TruffleBoundary
   public final Type getMetaObject(@Bind("$node") Node node) {
     return getType(node);
   }
