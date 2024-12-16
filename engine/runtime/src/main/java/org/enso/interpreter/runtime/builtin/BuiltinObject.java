75109063c5ca5084d2de8734594dc2ae06402a20
Pavel Marek <pavel.marek@enso.org>
2024-12-16 11:00:39 +0100

    [WIP] Try to remove builtin type caching in BuiltinObject
    Maybe this will fix the invalid polyglot layer sharing assertion errors.

--- engine/runtime/src/main/java/org/enso/interpreter/runtime/builtin/BuiltinObject.java
+++ engine/runtime/src/main/java/org/enso/interpreter/runtime/builtin/BuiltinObject.java
@@ -2,6 +2,7 @@ package org.enso.interpreter.runtime.builtin;
 
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.dsl.Bind;
 import com.oracle.truffle.api.interop.InteropLibrary;
 import com.oracle.truffle.api.interop.UnsupportedMessageException;
@@ -27,8 +28,6 @@ public abstract class BuiltinObject extends EnsoObject {
 
   private final String builtinName;
 
-  @CompilationFinal private Builtin cachedBuiltinType;
-
   /**
    * @param builtinName Simple name of the builtin that should be contained in {@link
    *     org.enso.interpreter.runtime.builtin.Builtins#builtinsByName}.
@@ -43,13 +42,11 @@ public abstract class BuiltinObject extends EnsoObject {
   }
 
   @ExportMessage
+  @TruffleBoundary
   public final Type getType(@Bind("$node") Node node) {
-    if (cachedBuiltinType == null) {
-      CompilerDirectives.transferToInterpreterAndInvalidate();
-      var ctx = EnsoContext.get(node);
-      cachedBuiltinType = ctx.getBuiltins().getBuiltinType(builtinName);
-    }
-    return cachedBuiltinType.getType();
+    var ctx = EnsoContext.get(node);
+    var builtinType = ctx.getBuiltins().getBuiltinType(builtinName);
+    return builtinType.getType();
   }
 
   /**
@@ -76,6 +73,7 @@ public abstract class BuiltinObject extends EnsoObject {
   }
 
   @ExportMessage
+  @TruffleBoundary
   public final Type getMetaObject(@Bind("$node") Node node) {
     return getType(node);
   }
