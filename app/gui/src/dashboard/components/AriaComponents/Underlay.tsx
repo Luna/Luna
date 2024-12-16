20a0142789793c6ca530b815eaada76796653b4a
Sergei Garin <sergei.garin@enso.org>
2024-12-16 14:53:14 +0400

    Fix docs for underaly element

--- app/gui/src/dashboard/components/AriaComponents/Underlay.tsx
+++ app/gui/src/dashboard/components/AriaComponents/Underlay.tsx
@@ -1,4 +1,9 @@
-/** @file An underlay that covers the entire screen. */
+/**
+ * @file
+ * Component that renders an blurry underlay, that matches our design guidelines
+ * Useful when you want to display a semi transparent element
+ * above the other one.
+ */
 import { DIALOG_BACKGROUND } from '#/components/AriaComponents/Dialog'
 import type { VariantProps } from '#/utilities/tailwindVariants'
 import type { HTMLAttributes } from 'react'
@@ -10,7 +15,11 @@ export interface UnderlayProps
   extends HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof DIALOG_BACKGROUND> {}
 
-/** An underlay that covers the entire screen. */
+/**
+ * Component that renders an blurry underlay, that matches our design guidelines
+ * Useful when you want to display a semi transparent element
+ * above the other one.
+ */
 export function Underlay(props: UnderlayProps) {
   const { className, variants = DIALOG_BACKGROUND, variant, ...rest } = props
 
