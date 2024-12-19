cbc514fadd5cd11b80ed2e2242bb1faaa0a55194
Paweł Grabarz <frizi09@gmail.com>
2024-12-19 12:15:36 +0100

    Skip env-replacer step for now.

--- app/gui/vite.config.ts
+++ app/gui/vite.config.ts
@@ -96,6 +96,7 @@ export default defineConfig({
       output: {
         manualChunks: {
           config: ['./src/config'],
+          entrypoint: ['./src/entrypoint'],
         },
       },
     },
