a66a0b2caf90dd6ab0b2f25e90d294a602bc3375
Sergei Garin <sergei.garin@enso.org>
2024-12-23 20:49:51 +0400

    Try fix unit tests

--- app/gui/src/dashboard/test/setup.ts
+++ app/gui/src/dashboard/test/setup.ts
@@ -5,11 +5,8 @@
 import * as matchers from '@testing-library/jest-dom/matchers'
 import { cleanup } from '@testing-library/react'
 import { MotionGlobalConfig } from 'framer-motion'
-// eslint-disable-next-line no-restricted-syntax
-import ResizeObserver from 'resize-observer-polyfill'
-import { afterEach, expect, vi } from 'vitest'
 
-vi.stubGlobal('ResizeObserver', ResizeObserver)
+import { afterEach, expect } from 'vitest'
 
 MotionGlobalConfig.skipAnimations = true
 
