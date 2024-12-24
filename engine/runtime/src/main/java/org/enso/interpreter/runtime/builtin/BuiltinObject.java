7969907da39bcb886220b8a3c102f1943c08cd52
Pavel Marek <pavel.marek@enso.org>
2024-12-24 13:24:44 +0100

    Replace Fallback with Specialization with replaces

--- engine/runtime/src/main/java/org/enso/interpreter/runtime/builtin/BuiltinObject.java
+++ engine/runtime/src/main/java/org/enso/interpreter/runtime/builtin/BuiltinObject.java
@@ -93,7 +93,7 @@ public abstract class BuiltinObject extends EnsoObject {
       return cachedBuiltinType.getType();
     }
 
-    @Fallback
+    @Specialization(replaces = "doItCached")
     public static Type uncached(BuiltinObject receiver, @Bind("$node") Node node) {
       var ctx = getCtx(node);
       return getBuiltinType(receiver, ctx).getType();
