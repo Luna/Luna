31c97b94928be03cbc8bb2f3eb985ceb03ef20fa
Dmitry Bushev <bushevdv@gmail.com>
2024-12-25 15:30:08 +0000

    DRAFT: insight parse tree

--- app/ydoc-server-polyglot/src/insight.ts
+++ app/ydoc-server-polyglot/src/insight.ts
@@ -1,16 +1,40 @@
+import { deserializeIdMap } from 'ydoc-server'
+import * as Ast from 'ydoc-shared/ast'
+import { splitFileContents } from 'ydoc-shared/ensoFile'
+import { ModuleDoc } from 'ydoc-shared/yjsModel'
+import * as Y from 'yjs'
+
 print("Initializing Insight: " + JSON.stringify(insight));
 
+const PROJECT_NAME = 'NewProject1'
+const doc = new ModuleDoc(new Y.Doc());
+const syncModule = new Ast.MutableModule(doc.ydoc)
+
+function parseContents(contents: string, syncModule: Ast.MutableModule) {
+  const { code, idMapJson, metadataJson } = splitFileContents(contents)
+  const parsedIdMap = deserializeIdMap(idMapJson)
+
+  const { root, spans } = Ast.parseModuleWithSpans(code, syncModule)
+  syncModule.setRoot(root)
+
+  Ast.setExternalIds(syncModule, spans, parsedIdMap)
+  print(`[parseContents] idMap.entries=${parsedIdMap.entries()}`)
+}
+
 insight.on("source", function(ctx, frame) {
-  print(`[source] ctx.uri=${ctx.uri}`)
+  print(`[source] ctx.uri=${ctx.name}`)
+  if (ctx.name.includes(PROJECT_NAME)) {
+    print(`[source] MATCHED ctx=${JSON.stringify(ctx)}`)
+    parseContents(ctx.characters, syncModule)
+  }
 });
 
 insight.on("return", function(ctx, frame) {
   print(`[return] ctx='${JSON.stringify(ctx)}'`)
   print(`[return] frame='${JSON.stringify(frame)}'`)
-  print(`[return] ctx.returnValue='${JSON.stringify(ctx.returnValue(frame))}'`)
 }, {
-  expressions: true,
+  expressions: false,
   statements: true,
-  roots: true,
+  roots: false,
   rootNameFilter: ".*main"
 });
