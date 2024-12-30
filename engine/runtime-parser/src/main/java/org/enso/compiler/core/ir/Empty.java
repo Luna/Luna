b82d7cf1024f8b58fd6dd69258f15c48490c139c
Pavel Marek <pavel.marek@enso.org>
2024-12-30 14:56:16 +0100

    Fix build

--- engine/runtime-parser/src/main/java/org/enso/compiler/core/ir/Empty.java
+++ engine/runtime-parser/src/main/java/org/enso/compiler/core/ir/Empty.java
@@ -3,7 +3,7 @@ package org.enso.compiler.core.ir;
 import org.enso.runtime.parser.dsl.GenerateFields;
 import org.enso.runtime.parser.dsl.GenerateIR;
 
-@GenerateIR(interfaces = "org.enso.compiler.core.ir.Expression")
+@GenerateIR(interfaces = {Expression.class})
 public final class Empty extends EmptyGen {
   @GenerateFields
   public Empty(IdentifiedLocation identifiedLocation, MetadataStorage passData) {
