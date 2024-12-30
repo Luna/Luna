f8c9e12d2757b3c535001868d94c5fc39b53303d
Pavel Marek <pavel.marek@enso.org>
2024-12-30 20:17:11 +0100

    mapExpressions duplicates this iff one of the mapped children changed

--- engine/runtime-parser-processor/src/main/java/org/enso/runtime/parser/processor/methodgen/MapExpressionsMethodGenerator.java
+++ engine/runtime-parser-processor/src/main/java/org/enso/runtime/parser/processor/methodgen/MapExpressionsMethodGenerator.java
@@ -1,10 +1,12 @@
 package org.enso.runtime.parser.processor.methodgen;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
-import javax.lang.model.element.Element;
+import java.util.stream.Collectors;
 import javax.lang.model.element.ExecutableElement;
-import javax.lang.model.element.TypeElement;
 import org.enso.runtime.parser.processor.GeneratedClassContext;
+import org.enso.runtime.parser.processor.GeneratedClassContext.ClassField;
 import org.enso.runtime.parser.processor.IRProcessingException;
 import org.enso.runtime.parser.processor.field.Field;
 import org.enso.runtime.parser.processor.utils.Utils;
@@ -50,109 +52,149 @@ public final class MapExpressionsMethodGenerator {
 
     var children = ctx.getUserFields().stream().filter(Field::isChild);
     var newChildren =
-        children.map(
-            child -> {
-              ExecutableElement childsMapExprMethod;
-              if (child.isList() || child.isOption()) {
-                childsMapExprMethod =
-                    Utils.findMapExpressionsMethod(
-                        child.getTypeParameter(), ctx.getProcessingEnvironment());
-              } else {
-                childsMapExprMethod =
-                    Utils.findMapExpressionsMethod(child.getType(), ctx.getProcessingEnvironment());
-              }
+        children
+            .map(
+                child -> {
+                  ExecutableElement childsMapExprMethod;
+                  if (child.isList() || child.isOption()) {
+                    childsMapExprMethod =
+                        Utils.findMapExpressionsMethod(
+                            child.getTypeParameter(), ctx.getProcessingEnvironment());
+                  } else {
+                    childsMapExprMethod =
+                        Utils.findMapExpressionsMethod(
+                            child.getType(), ctx.getProcessingEnvironment());
+                  }
 
-              var typeUtils = ctx.getProcessingEnvironment().getTypeUtils();
-              var childsMapExprMethodRetType =
-                  typeUtils.asElement(childsMapExprMethod.getReturnType());
-              var shouldCast =
-                  !typeUtils.isSameType(
-                      child.getType().asType(), childsMapExprMethodRetType.asType());
-              if (child.isList() || child.isOption()) {
-                shouldCast = false;
-              }
+                  var typeUtils = ctx.getProcessingEnvironment().getTypeUtils();
+                  var childsMapExprMethodRetType =
+                      typeUtils.asElement(childsMapExprMethod.getReturnType());
+                  var shouldCast =
+                      !typeUtils.isSameType(
+                          child.getType().asType(), childsMapExprMethodRetType.asType());
+                  if (child.isList() || child.isOption()) {
+                    shouldCast = false;
+                  }
 
-              String newChildType = typeName(childsMapExprMethodRetType);
-              if (child.isList()) {
-                newChildType = "List<" + newChildType + ">";
-              } else if (child.isOption()) {
-                newChildType = "Option<" + newChildType + ">";
-              }
+                  String newChildType = childsMapExprMethodRetType.getSimpleName().toString();
+                  if (child.isList()) {
+                    newChildType = "List<" + newChildType + ">";
+                  } else if (child.isOption()) {
+                    newChildType = "Option<" + newChildType + ">";
+                  }
 
-              var newChildName = child.getName() + "Mapped";
-              sb.append("  ").append(newChildType).append(" ").append(newChildName);
-              if (child.isNullable()) {
-                sb.append(" = null;").append(System.lineSeparator());
-                sb.append("  if (")
-                    .append(child.getName())
-                    .append(" != null) {")
-                    .append(System.lineSeparator());
-                // childMapped = child.mapExpressions(fn);
-                sb.append("    ")
-                    .append(newChildName)
-                    .append(".")
-                    .append(METHOD_NAME)
-                    .append("(fn);")
-                    .append(System.lineSeparator());
-                sb.append("  }").append(System.lineSeparator());
-              } else {
-                if (!child.isList() && !child.isOption()) {
-                  // ChildType childMapped = child.mapExpressions(fn);
-                  sb.append(" = ")
-                      .append(child.getName())
-                      .append(".")
-                      .append(METHOD_NAME)
-                      .append("(fn);")
-                      .append(System.lineSeparator());
-                } else {
-                  Utils.hardAssert(child.isList() || child.isOption());
-                  // List<ChildType> childMapped = child.map(e -> e.mapExpressions(fn));
-                  sb.append(" = ")
-                      .append(child.getName())
-                      .append(".map(e -> e.")
-                      .append(METHOD_NAME)
-                      .append("(fn));")
-                      .append(System.lineSeparator());
-                }
-              }
-              return new MappedChild(newChildName, child, shouldCast);
-            });
-    sb.append("  ").append("var bldr = new Builder(this);").append(System.lineSeparator());
-    newChildren.forEach(
-        newChild -> {
-          if (newChild.shouldCast) {
-            sb.append("  ")
-                .append("if (!(")
-                .append(newChild.newChildName)
-                .append(" instanceof ")
-                .append(newChild.child.getType().getSimpleName())
-                .append(")) {")
-                .append(System.lineSeparator());
-            sb.append("    ")
-                .append(
-                    "throw new IllegalStateException(\"Duplicated child is not of the expected"
-                        + " type: \" + ")
-                .append(newChild.newChildName)
-                .append(");")
-                .append(System.lineSeparator());
-            sb.append("  }").append(System.lineSeparator());
-          }
-          sb.append("  ").append("bldr.").append(newChild.child.getName()).append("(");
-          if (newChild.shouldCast) {
-            sb.append("(").append(newChild.child.getType().getSimpleName()).append(") ");
-          }
-          sb.append(newChild.newChildName).append(");").append(System.lineSeparator());
-        });
-    sb.append("  return bldr.build();").append(System.lineSeparator());
+                  var newChildName = child.getName() + "Mapped";
+                  sb.append("  ").append(newChildType).append(" ").append(newChildName);
+                  if (child.isNullable()) {
+                    sb.append(" = null;").append(System.lineSeparator());
+                    sb.append("  if (")
+                        .append(child.getName())
+                        .append(" != null) {")
+                        .append(System.lineSeparator());
+                    // childMapped = child.mapExpressions(fn);
+                    sb.append("    ")
+                        .append(newChildName)
+                        .append(".")
+                        .append(METHOD_NAME)
+                        .append("(fn);")
+                        .append(System.lineSeparator());
+                    sb.append("  }").append(System.lineSeparator());
+                  } else {
+                    if (!child.isList() && !child.isOption()) {
+                      // ChildType childMapped = child.mapExpressions(fn);
+                      sb.append(" = ")
+                          .append(child.getName())
+                          .append(".")
+                          .append(METHOD_NAME)
+                          .append("(fn);")
+                          .append(System.lineSeparator());
+                    } else {
+                      Utils.hardAssert(child.isList() || child.isOption());
+                      // List<ChildType> childMapped = child.map(e -> e.mapExpressions(fn));
+                      sb.append(" = ")
+                          .append(child.getName())
+                          .append(".map(e -> e.")
+                          .append(METHOD_NAME)
+                          .append("(fn));")
+                          .append(System.lineSeparator());
+                    }
+                  }
+                  return new MappedChild(newChildName, child, shouldCast);
+                })
+            .toList();
+    if (newChildren.isEmpty()) {
+      sb.append("  return ")
+          .append("(")
+          .append(ctx.getProcessedClass().getClazz().getSimpleName().toString())
+          .append(") this;")
+          .append(System.lineSeparator());
+      sb.append("}").append(System.lineSeparator());
+      return sb.toString();
+    }
+    sb.append("  // Only copy if some of the children actually changed")
+        .append(System.lineSeparator());
+    var changedCond =
+        newChildren.stream()
+            .map(newChild -> newChild.newChildName + " != " + newChild.child.getName())
+            .collect(Collectors.joining(" || "));
+    sb.append("  ").append("if (").append(changedCond).append(") {").append(System.lineSeparator());
+    sb.append("    ").append("var bldr = new Builder();").append(System.lineSeparator());
+    for (MappedChild newChild : newChildren) {
+      if (newChild.shouldCast) {
+        sb.append("    ")
+            .append("if (!(")
+            .append(newChild.newChildName)
+            .append(" instanceof ")
+            .append(newChild.child.getType().getSimpleName())
+            .append(")) {")
+            .append(System.lineSeparator());
+        sb.append("      ")
+            .append(
+                "throw new IllegalStateException(\"Duplicated child is not of the expected"
+                    + " type: \" + ")
+            .append(newChild.newChildName)
+            .append(");")
+            .append(System.lineSeparator());
+        sb.append("    }").append(System.lineSeparator());
+      }
+      sb.append("    ").append("bldr.").append(newChild.child.getName()).append("(");
+      if (newChild.shouldCast) {
+        sb.append("(").append(newChild.child.getType().getSimpleName()).append(") ");
+      }
+      sb.append(newChild.newChildName).append(");").append(System.lineSeparator());
+    }
+    for (var field : restOfTheFields(newChildren)) {
+      sb.append("    ")
+          .append("bldr.")
+          .append(field.name())
+          .append("(")
+          .append(field.name())
+          .append(");")
+          .append(System.lineSeparator());
+    }
+    sb.append("    return bldr.build();").append(System.lineSeparator());
+    sb.append("  } else { ").append(System.lineSeparator());
+    sb.append("    // None of the mapped children changed - just return this")
+        .append(System.lineSeparator());
+    sb.append("    return ")
+        .append("(")
+        .append(ctx.getProcessedClass().getClazz().getSimpleName().toString())
+        .append(") this;")
+        .append(System.lineSeparator());
+    sb.append("  }").append(System.lineSeparator());
     sb.append("}").append(System.lineSeparator());
     return sb.toString();
   }
 
-  private String typeName(Element element) {
-    if (element instanceof TypeElement typeElement) {
-      return typeElement.getQualifiedName().toString();
+  private List<ClassField> restOfTheFields(List<MappedChild> newChildren) {
+    var restOfFields = new ArrayList<ClassField>();
+    for (var field : ctx.getAllFields()) {
+      if (newChildren.stream()
+          .noneMatch(newChild -> newChild.child.getName().equals(field.name()))) {
+        restOfFields.add(field);
+      }
     }
-    return element.getSimpleName().toString();
+    return restOfFields;
   }
 
   private record MappedChild(String newChildName, Field child, boolean shouldCast) {}
