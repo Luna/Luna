0393b222363cd8e1cb1e2fbdc89fb201ae5486f5
Jaroslav Tulach <jaroslav.tulach@enso.org>
2025-01-09 07:57:31 +0100

    Filling in stack trace when PanicException is caught fixes the test

--- engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/error/CatchPanicNode.java
+++ engine/runtime/src/main/java/org/enso/interpreter/node/expression/builtin/error/CatchPanicNode.java
@@ -1,5 +1,6 @@
 package org.enso.interpreter.node.expression.builtin.error;
 
+import com.oracle.truffle.api.TruffleStackTrace;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.exception.AbstractTruffleException;
@@ -89,6 +90,12 @@ public abstract class CatchPanicNode extends Node {
     if (profile.profile(isValueOfTypeNode.execute(panicType, payload, true))) {
       var builtins = EnsoContext.get(this).getBuiltins();
       var cons = builtins.caughtPanic().getUniqueConstructor();
+      // if (originalException instanceof PanicException panic) {
+      //   panic.caughtBy(this);
+      // } else
+      {
+        TruffleStackTrace.fillIn(originalException);
+      }
       var caughtPanic =
           AtomNewInstanceNode.getUncached().newInstance(cons, payload, originalException);
       return invokeCallableNode.execute(handler, frame, state, new Object[] {caughtPanic});
