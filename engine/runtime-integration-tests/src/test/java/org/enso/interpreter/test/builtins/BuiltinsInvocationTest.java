84f4112859ded171597d2779be8a3b49e5197368
Pavel Marek <pavel.marek@enso.org>
2025-01-13 18:28:31 +0100

    Error.catch_primitive throws type error on wrong self arg

--- engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/builtins/BuiltinsInvocationTest.java
+++ engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/builtins/BuiltinsInvocationTest.java
@@ -1,11 +1,12 @@
 package org.enso.interpreter.test.builtins;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
 
 import org.enso.test.utils.ContextUtils;
 import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.PolyglotException;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -31,9 +32,17 @@ public class BuiltinsInvocationTest {
         from Standard.Base import all
 
         main =
-            Error.catch Any (Error.throw "No longer valid") x->x
+            (Error.catch_primitive self=(y->y)) (x->x)
         """;
-    var res = ContextUtils.evalModule(ctx, src);
-    assertThat("Should not crash - should return something", res, is(notNullValue()));
+    try {
+      ContextUtils.evalModule(ctx, src);
+    } catch (PolyglotException e) {
+      var panic = e.getGuestObject();
+      assertThat("Should be panic", panic.isException());
+      assertThat(
+          "Should have Type error as payload",
+          e.getMessage(),
+          allOf(containsString("Type error"), containsString("Dataflow_Error")));
+    }
   }
 }
